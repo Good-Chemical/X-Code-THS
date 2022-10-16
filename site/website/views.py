2a5dad9dbea3057a6466929bff2c773fee3af4f2
Good-Chemical <115777905+Good-Chemical@users.noreply.github.com>
2022-10-16 08:41:38 +0530

    Add files via upload

---
+++ site/website/views.py
@@ -0,0 +1,9 @@
+from xmlrpc.client import boolean
+from django.shortcuts import render
+from flask import Blueprint,render_template
+
+views=Blueprint('views',__name__)
+
+@views.route('/')
+def home():
+    return render_template("SC.html",boolean=True)
